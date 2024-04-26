f92ebf5855b99cc1e22df49edaff06c4a7ba4c78
Modadeoluwa <modadegospel@gmail.com>
2024-04-26 17:45:08 +0100

    Added size change functionality

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 const myContainer = document.querySelector('.parent-div');
+const userInput = document.querySelector('#numberInput');
 
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement("div");
-    myContainer.append(div);
-    div.classList.add("square-divs");
+function createSketch () {
+    for (let i = 0; i < 256; i++) {
+        let newElement = document.createElement("div");
+        newElement.classList.add("grid-square")
+        myContainer.append(newElement);
+    }
 }
+
+function updateGrid () {
+    for (let i = 0; i < userInput.value * userInput.value; i++) {
+        let newElement = document.createElement("div");
+        newElement.classList.add("grid-square")
+        myContainer.append(newElement);
+    }
+}
+
+userInput.addEventListener("change", updateGrid)
+createSketch();
 
 function generateRandomColor() {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
-
+    
     return `rgb(${r}, ${g}, ${b})`;
 };
 
-let colorChange = document.querySelectorAll(".square-divs");
+let colorChange = document.querySelectorAll(".grid-square");
 
 colorChange.forEach((div) => {
     div.addEventListener('mouseenter', () => {
